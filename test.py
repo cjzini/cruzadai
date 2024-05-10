8dccdd23cc653792c88469b403a77177fb3f4911
cjzini <cjzini@gmail.com>
2024-05-10 16:46:38 -0300

    altera

--- test.py
+++ test.py
@@ -2,7 +2,13 @@ from cruzada.control import Gerador
 from config import *
 import google.generativeai as genai
 
+
+# Por questões de seguranca, a API_KEY está armazenada em arquivo sem ser enviado para o GitHub
 genai.configure(api_key=API_KEY)
+
+
+
+
 #Gerar testes
 gerador = Gerador('/uploads/cruzada1.txt')
 gerador.gera_quadro()
